57d69206517d6fdad09d1a3f674914800b8d76da
HyonNeo <128829684+HyonNeo@users.noreply.github.com>
2023-05-11 13:26:34 -0400

    added essentials files and the api of weather

---
+++ Assets/js/script.js
@@ -0,0 +1,3 @@
+let url = [":https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=04acb5a8b4056c5cf9ae401ef658f9e8"];
+
+fetch(url).then(function(response) { return response.json()}).then(function(data) { console.log(data)})
